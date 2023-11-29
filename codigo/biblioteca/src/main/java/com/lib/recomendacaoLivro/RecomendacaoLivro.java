9015138934dbfb3215365ec94828500b8a60b993
Jully Silva <jullyksilva@hotmail.com>
2023-11-29 11:23:13 -0300

    recomendacao

---
+++ codigo/biblioteca/src/main/java/com/lib/recomendacaoLivro/RecomendacaoLivro.java
@@ -0,0 +1,14 @@
+package com.lib.recomendacaoLivro;
+
+import com.lib.biblioteca.Biblioteca;
+import com.lib.usuario.Usuario;
+
+public class RecomendacaoLivro {
+    
+    //TODO: passar a lib inteira e filtrar ou passar o array de livros já filtrado.
+    
+
+    public void recomendar(Usuario usuario, Biblioteca biblioteca) {
+
+    }
+}
